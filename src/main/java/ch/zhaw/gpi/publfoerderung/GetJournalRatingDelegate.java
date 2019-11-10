32fdf180ef33509d568a45102e498dec81c36941
Björn Scheppler <scep@zhaw.ch>
2019-11-10 15:55:57 +0100

    Aufgabe 8 abgeschlossen

--- src/main/java/ch/zhaw/gpi/publfoerderung/GetJournalRatingDelegate.java
+++ src/main/java/ch/zhaw/gpi/publfoerderung/GetJournalRatingDelegate.java
@@ -1,12 +1,11 @@
 package ch.zhaw.gpi.publfoerderung;
 
-import java.util.List;
-
 import javax.inject.Named;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * GetJournalRatingDelegate
@@ -14,20 +13,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Named("getJournalRatingAdapter")
 public class GetJournalRatingDelegate implements JavaDelegate {
 
-    @Autowired
-    private JournalRepository journalRepository;
-
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         String journalTitle = (String) execution.getVariable("journalTitle");
 
-        List<JournalEntity> journals = journalRepository.findByJournalTitleIgnoreCase(journalTitle);
+        RestTemplate restTemplate = new RestTemplate();
 
         Integer journalRating;
-        if(journals.size() == 0){
+
+        try{
+            Journal journal = restTemplate.getForObject("http://localhost:8090/aacsbapi/v1/journals/{journalTitle}", Journal.class, journalTitle);
+
+            if(journal == null){
+                journalRating = 0;
+            } else {
+                journalRating = journal.getJournalRating();
+            }
+        } catch(HttpClientErrorException e) {
             journalRating = 0;
-        } else {
-            journalRating = journals.get(0).getJournalRating();
         }
 
         execution.setVariable("journalAacsbRating", journalRating);
